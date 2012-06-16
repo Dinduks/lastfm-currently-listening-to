0d02d03cc11eb50dfad96c39bacff72a67a76bb6
Dinduks <samy@dindane.com>
2012-06-16 21:35:27 +0200

    Add a config file (stupid PHP)

--- src/app.php
+++ src/app.php
@@ -8,6 +8,8 @@ $loader = require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Application();
 
+require_once __DIR__.'/config.php';
+
 $app->register(new TwigServiceProvider(), array('twig.path'    => __DIR__.'/views'));
 
 $app->error(function (\Exception $e, $code) use ($app) {
@@ -19,11 +21,9 @@ $app->error(function (\Exception $e, $code) use ($app) {
     return new Response($app['twig']->render('error.html.twig', array('error' => $error)), $code);
 });
 
-$app->before(function() use ($app) {
-    if (isset($_ENV['LASTFM_API_KEY']))
-        $app['lastfmApiKey'] = $_ENV['LASTFM_API_KEY'];
-    else if (apache_getenv('LASTFM_API_KEY'))
-        $app['lastfmApiKey'] = apache_getenv('LASTFM_API_KEY');
+$app->before(function() use ($app, $lastfmApiKey) {
+    if (isset($lastfmApiKey))
+        $app['lastfmApiKey'] = $lastfmApiKey;
     else
         return new Response($app['twig']->render('error.html.twig', array('error' => 'No Last.fm API key found!')));
 });
